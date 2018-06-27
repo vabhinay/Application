9292f5cd79f47562d29da7498ed6d8779c4f9916
Abhinay Verma <abhinay.verma76@gmail.com>
2018-06-27 21:23:38 +0530

    Add home module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,49 +1,53 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ContainerComponent } from './container/container.component';
 import { CourseCubeComponent } from './container/course-cube/course-cube.component';
 import { LeftNavComponent } from './container/left-nav/left-nav.component';
-import { HttpClientModule } from '@angular/common/http';
 import { CourseCubeService } from './service/course-cube.service';
 import { CardComponent } from './container/course-cube/card/card.component';
+import { HomeComponent } from './home/home.component';
 
-const appRoute: Routes=[
-{
-  path: 'course-cube',
-  component: CourseCubeComponent
-},
-{
-  path: '',
-  redirectTo: '/course-cube',
-  pathMatch: 'full'
-},
-{
-  path: '**',
-  component: CourseCubeComponent
-}
+const appRoute :Routes=[
+  {
+    path:'home', component:HomeComponent,
+  },
+  {
+    path:'course-cube', component:ContainerComponent,
+    children:[
+      {
+        path:'',component:CourseCubeComponent
+      }
+    ]
+  },
+  {
+    path:'', redirectTo:'/course-cube',pathMatch:'full'
+  },
+  {
+    path:'**', component:CourseCubeComponent
+  }
 ]
+
 @NgModule({
   declarations: [
     AppComponent,
+    ContainerComponent,
     HeaderComponent,
     FooterComponent,
-    ContainerComponent,
     CourseCubeComponent,
     LeftNavComponent,
-    CardComponent
-    
+    CardComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoute),
     HttpClientModule
-
   ],
   providers: [CourseCubeService],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CourseCubeService } from '../service/course-cube.service';
 
 @Component({
   selector: 'cc-home',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  courseCubeList = [];
+  constructor(public courseCubeService:CourseCubeService) { }
 
   ngOnInit() {
+    this.getCourseCube();
+    
+  }
+
+  getCourseCube(){
+    this.courseCubeService.getCourseCube().subscribe(
+      //Success
+      (courseCubeResponse) => {
+        this.courseCubeList = courseCubeResponse ? courseCubeResponse["courseCube"] :[];
+        this.courseCubeService.setCourseCubeList(this.courseCubeList);
+      },
+      // error
+      (error) => {
+        console.log(error);
+      },
+      // finally
+      ()=>{
+        console.log('finally');
+      });
   }
 
 }
